5ea845a941a94b1ea275370903fbe38d0242d10f
jjz <woaf1003@gmail.com>
2018-09-21 20:36:00 +0800

    add abc-rpc.py

---
+++ test_framework/blocktools.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015-2016 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Utilities for manipulating blocks and transactions."""
+
+from .mininode import *
+from .script import CScript, OP_TRUE, OP_CHECKSIG, OP_RETURN, OP_PUSHDATA2, OP_DUP, OP_HASH160, OP_EQUALVERIFY
+from .mininode import CTransaction, CTxOut, CTxIn
+from .util import satoshi_round
+
+# Create a block (with regtest difficulty)
+
+
+def create_block(hashprev, coinbase, nTime=None):
+    block = CBlock()
+    if nTime is None:
+        import time
+        block.nTime = int(time.time() + 600)
+    else:
+        block.nTime = nTime
+    block.hashPrevBlock = hashprev
+    block.nBits = 0x207fffff  # Will break after a difficulty adjustment...
+    block.vtx.append(coinbase)
+    block.hashMerkleRoot = block.calc_merkle_root()
+    block.calc_sha256()
+    return block
+
+
+def serialize_script_num(value):
+    r = bytearray(0)
+    if value == 0:
+        return r
+    neg = value < 0
+    absvalue = -value if neg else value
+    while (absvalue):
+        r.append(int(absvalue & 0xff))
+        absvalue >>= 8
+    if r[-1] & 0x80:
+        r.append(0x80 if neg else 0)
+    elif neg:
+        r[-1] |= 0x80
+    return r
+
+# Create a coinbase transaction, assuming no miner fees.
+# If pubkey is passed in, the coinbase output will be a P2PK output;
+# otherwise an anyone-can-spend output.
+
+
+def create_coinbase(height, pubkey=None):
+    coinbase = CTransaction()
+    coinbase.vin.append(CTxIn(COutPoint(0, 0xffffffff),
+                              ser_string(serialize_script_num(height)), 0xffffffff))
+    coinbaseoutput = CTxOut()
+    coinbaseoutput.nValue = 50 * COIN
+    halvings = int(height / 150)  # regtest
+    coinbaseoutput.nValue >>= halvings
+    if (pubkey != None):
+        coinbaseoutput.scriptPubKey = CScript([pubkey, OP_CHECKSIG])
+    else:
+        coinbaseoutput.scriptPubKey = CScript([OP_TRUE])
+    coinbase.vout = [coinbaseoutput]
+
+    # Make sure the coinbase is at least 100 bytes
+    coinbase_size = len(coinbase.serialize())
+    if coinbase_size < 100:
+        coinbase.vin[0].scriptSig += b'x' * (100 - coinbase_size)
+
+    coinbase.calc_sha256()
+    return coinbase
+
+# Create a transaction.
+# If the scriptPubKey is not specified, make it anyone-can-spend.
+
+
+def create_transaction(prevtx, n, sig, value, scriptPubKey=CScript()):
+    tx = CTransaction()
+    assert(n < len(prevtx.vout))
+    tx.vin.append(CTxIn(COutPoint(prevtx.sha256, n), sig, 0xffffffff))
+    tx.vout.append(CTxOut(value, scriptPubKey))
+    tx.calc_sha256()
+    return tx
+
+
+def get_legacy_sigopcount_block(block, fAccurate=True):
+    count = 0
+    for tx in block.vtx:
+        count += get_legacy_sigopcount_tx(tx, fAccurate)
+    return count
+
+
+def get_legacy_sigopcount_tx(tx, fAccurate=True):
+    count = 0
+    for i in tx.vout:
+        count += i.scriptPubKey.GetSigOpCount(fAccurate)
+    for j in tx.vin:
+        # scriptSig might be of type bytes, so convert to CScript for the moment
+        count += CScript(j.scriptSig).GetSigOpCount(fAccurate)
+    return count
+
+
+def create_confirmed_utxos(node, count, age=101):
+    """
+    Helper to create at least "count" utxos
+    """
+    to_generate = int(0.5 * count) + age
+    while to_generate > 0:
+        node.generate(min(25, to_generate))
+        to_generate -= 25
+    utxos = node.listunspent()
+    iterations = count - len(utxos)
+    addr1 = node.getnewaddress()
+    addr2 = node.getnewaddress()
+    if iterations <= 0:
+        return utxos
+    for i in range(iterations):
+        t = utxos.pop()
+        inputs = []
+        inputs.append({"txid": t["txid"], "vout": t["vout"]})
+        outputs = {}
+        outputs[addr1] = satoshi_round(t['amount'] / 2)
+        outputs[addr2] = satoshi_round(t['amount'] / 2)
+        raw_tx = node.createrawtransaction(inputs, outputs)
+        ctx = FromHex(CTransaction(), raw_tx)
+        fee = node.calculate_fee(ctx) // 2
+        ctx.vout[0].nValue -= fee
+        # Due to possible truncation, we go ahead and take another satoshi in
+        # fees to ensure the transaction gets through
+        ctx.vout[1].nValue -= fee + 1
+        signed_tx = node.signrawtransaction(ToHex(ctx))["hex"]
+        node.sendrawtransaction(signed_tx)
+
+    while (node.getmempoolinfo()['size'] > 0):
+        node.generate(1)
+
+    utxos = node.listunspent()
+    assert(len(utxos) >= count)
+    return utxos
+
+
+def mine_big_block(node, utxos=None):
+    # generate a 66k transaction,
+    # and 14 of them is close to the 1MB block limit
+    num = 14
+    utxos = utxos if utxos is not None else []
+    if len(utxos) < num:
+        utxos.clear()
+        utxos.extend(node.listunspent())
+    send_big_transactions(node, utxos, num, 100)
+    node.generate(1)
+
+
+def send_big_transactions(node, utxos, num, fee_multiplier):
+    from .cashaddr import decode
+    txids = []
+    padding = "1"*(512*127)
+    addrHash = decode(node.getnewaddress())[2]
+
+    for _ in range(num):
+        ctx = CTransaction()
+        utxo = utxos.pop()
+        txid = int(utxo['txid'], 16)
+        ctx.vin.append(CTxIn(COutPoint(txid, int(utxo["vout"])), b""))
+        ctx.vout.append(CTxOut(0, CScript(
+            [OP_RETURN, OP_PUSHDATA2, len(padding), bytes(padding, 'utf-8')])))
+        ctx.vout.append(
+            CTxOut(int(satoshi_round(utxo['amount']*COIN)),
+                   CScript([OP_DUP, OP_HASH160, addrHash, OP_EQUALVERIFY, OP_CHECKSIG])))
+        # Create a proper fee for the transaction to be mined
+        ctx.vout[1].nValue -= int(fee_multiplier * node.calculate_fee(ctx))
+        signresult = node.signrawtransaction(
+            ToHex(ctx), None, None, "NONE|FORKID")
+        txid = node.sendrawtransaction(signresult["hex"], True)
+        txids.append(txid)
+    return txids
---
+++ test_framework/cdefs.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+Imports some application default values from source files outside the test
+framework, and defines equivalents of consensus parameters for the test
+framework.
+"""
+
+import os
+import re
+
+
+def get_srcdir():
+    """
+    Try to find out the base folder containing the 'src' folder.
+    If SRCDIR is set it does a sanity check and returns that.
+    Otherwise it goes on a search and rescue mission.
+
+    Returns None if it cannot find a suitable folder.
+    """
+    def contains_src(path_to_check):
+        if not path_to_check:
+            return False
+        else:
+            cand_path = os.path.join(path_to_check, 'src')
+            return os.path.exists(cand_path) and os.path.isdir(cand_path)
+
+    srcdir = os.environ.get('SRCDIR', '')
+    if contains_src(srcdir):
+        return srcdir
+
+    # Try to work it based out on main module
+    import sys
+    mainmod = sys.modules['__main__']
+    mainmod_path = getattr(mainmod, '__file__', '')
+    if mainmod_path and mainmod_path.endswith('.py'):
+        maybe_top = mainmod_path
+        while maybe_top != '/':
+            maybe_top = os.path.abspath(os.path.dirname(maybe_top))
+            if contains_src(maybe_top):
+                return maybe_top
+
+    # No luck, give up.
+    return None
+
+
+# Slurp in consensus.h contents
+_consensus_h_fh = open(os.path.join(get_srcdir(), 'src', 'consensus',
+                                    'consensus.h'), 'rt')
+_consensus_h_contents = _consensus_h_fh.read()
+_consensus_h_fh.close()
+
+# This constant is currently needed to evaluate some that are formulas
+ONE_MEGABYTE = 1000000
+
+# Extract relevant default values parameters
+
+# The maximum allowed block size before the fork
+LEGACY_MAX_BLOCK_SIZE = ONE_MEGABYTE
+
+# Default setting for maximum allowed size for a block, in bytes
+DEFAULT_MAX_BLOCK_SIZE = eval(
+    re.search(r'DEFAULT_MAX_BLOCK_SIZE = (.+);',
+              _consensus_h_contents).group(1))
+
+# The following consensus parameters should not be automatically imported.
+# They *should* cause test failures if application code is changed in ways
+# that violate current consensus.
+
+# The maximum allowed number of signature check operations per MB in a block
+# (network rule)
+MAX_BLOCK_SIGOPS_PER_MB = 20000
+
+# The maximum allowed number of signature check operations per transaction
+# (network rule)
+MAX_TX_SIGOPS_COUNT = 20000
+
+# The maximum number of sigops we're willing to relay/mine in a single tx
+# (policy.h constant)
+MAX_STANDARD_TX_SIGOPS = MAX_TX_SIGOPS_COUNT // 5
+
+# Coinbase transaction outputs can only be spent after this number of new
+# blocks (network rule)
+COINBASE_MATURITY = 100
+
+# Minimum size a transaction can have.
+MIN_TX_SIZE = 100
+
+if __name__ == "__main__":
+    # Output values if run standalone to verify
+    print("DEFAULT_MAX_BLOCK_SIZE = %d (bytes)" % DEFAULT_MAX_BLOCK_SIZE)
+    print("MAX_BLOCK_SIGOPS_PER_MB = %d (sigops)" % MAX_BLOCK_SIGOPS_PER_MB)
+    print("MAX_TX_SIGOPS_COUNT = %d (sigops)" % MAX_TX_SIGOPS_COUNT)
+    print("COINBASE_MATURITY = %d (blocks)" % COINBASE_MATURITY)
