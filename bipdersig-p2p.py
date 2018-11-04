4919790273a09d2562a350892bd7731254ca9345
Hongmin Wang <whunmr@gmail.com>
2018-11-04 14:28:52 +0800

    extend timeout of bipdersig-p2p.py to avoid generate blocks timeout on low performance CI server

--- bipdersig-p2p.py
+++ bipdersig-p2p.py
@@ -58,6 +58,10 @@ class BIP66Test(BitcoinTestFramework):
             ['--promiscuousmempoolflags=1', '--whitelist=127.0.0.1']]
         self.setup_clean_chain = True
 
+    def setup_nodes(self):
+        self.add_nodes(self.num_nodes, self.extra_args, timewait=300)
+        self.start_nodes()
+
     def run_test(self):
         node0 = NodeConnCB()
         connections = []
