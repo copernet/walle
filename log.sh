c05a02ced3ac6034d0d199fa2fff120f3186aca9
zymap <734206637@qq.com>
2018-11-13 19:45:57 +0800

    Fix log.sh, add operation 'push'

--- log.sh
+++ log.sh
@@ -4,3 +4,4 @@ jobID=$1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 containerID=`docker ps | awk '{if(NR==2){print $1}}'`
 docker commit $containerID copernet/copernicus:$jobID
+docker push copernet/copernicus:$jobID
